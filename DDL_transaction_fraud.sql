edf20bf5ce229b6e3b97cadb8335961db5e462e3
THEMANNICHOLAS <stingyblue@gmail.com>
2025-03-13 13:07:19 -0400

    Add DDL SQL code of current ER diagram

---
+++ DDL_transaction_fraud.sql
@@ -0,0 +1,73 @@
+--TO DO: Add more check to each attribute and determine
+--whether or not each attribute has enough space allocated.
+
+
+
+CREATE TABLE cardholder (
+	cardholder_id INT PRIMARY KEY,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	--DOB must be a valid date of birth 
+	dob DATE NOT NULL CHECK (dob < CURRENT_DATE AND dob > '1900-01-01'),
+	--User MUST choose an option, at the very least other. ENUM is best suited for this
+	gender ENUM('male', 'female', 'nonbinary', 'other') NOT NULL
+	
+);
+
+
+--Created a state code to save space and repititon.
+--Will need to be populated before used
+CREATE TABLE State (
+    state_code CHAR(2) PRIMARY KEY,
+    state_name VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE cardholder_location (
+    location_id INT PRIMARY KEY,
+    cardholder_id INT,
+    street VARCHAR(32) NOT NULL,
+    city VARCHAR(16) NOT NULL,
+    state_code CHAR(2) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+    latitude DECIMAL(10,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL,
+    city_pop NUMERIC(8,0) NOT NULL,
+    FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),
+    FOREIGN KEY (state_code) REFERENCES State(state_code)
+);
+
+
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY,
+    cardholder_id INT NOT NULL,
+	merchant_id INT NOT NULL,
+    transaction_date DATE NOT NULL,
+    amount NUMERIC(6,2) CHECK (amount >= 0),
+    unix_time INT NOT NULL,
+    is_fraud BOOL,
+    FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),
+	FOREIGN KEY (merchant_id) REFERENCES merchant (merchant_id)
+);
+
+
+CREATE TABLE merchant (
+	merchant_id INT PRIMARY KEY,
+	merchant_name VARCHAR(32) NOT NULL,
+	merchant_cat_id INT,
+	merchant_lat DECIMAL(10,8) NOT NULL,
+	merchant_long DECIMAL(11,8) NOT NULL,
+
+	FOREIGN KEY (merchant_cat_id) REFERENCES merchant_category (merchant_cat_id)
+);
+
+--Created table to store merchant categories to avoid repitition.
+--Each merchant is a one-to-one relationship.
+--Must be populated before using
+CREATE TABLE merchant_category (
+	merchant_cat_id INT PRIMARY KEY,
+	merchant_id INT,
+	category_name VARCHAR(32) NOT NULL,
+	
+	FOREIGN KEY (merchant_id) REFERENCES merchant (merchant_id)
+);
