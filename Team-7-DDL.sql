569799b51fd46c45d0529d9149a4c45b8f68f5af
THEMANNICHOLAS <stingyblue@gmail.com>
2025-04-07 13:39:17 -0400

    Fix normalization inconsistency with cardholder
    Fixed the normalization inconsistency with cardholder_location and city_details.

--- Team-7-DDL.sql
+++ Team-7-DDL.sql
@@ -56,14 +56,11 @@ CREATE TABLE cardholder_location (
     location_id INT PRIMARY KEY,	-- Unique identifier for each location entry
     cardholder_id INT,				-- Foreign key linking to the cardholder table
     street VARCHAR(50) NOT NULL,	-- Street address of the cardholder
-    city VARCHAR(50) NOT NULL,		-- City where the cardholder resides
-    state_code CHAR(2) NOT NULL,	-- State code (linked to the states table)
-    zip_code VARCHAR(10) NOT NULL,	-- Zip code of the cardholder's residence
+	city_id INT NOT NULL,
     latitude DECIMAL(10,8) NOT NULL,	-- Geographical latitude of the cardholder’s home
     longitude DECIMAL(11,8) NOT NULL,	-- Geographical longitude of the cardholder’s home
-    city_pop NUMERIC(8,0) NOT NULL,	-- Population of the city where the cardholder lives
     FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),	-- Ensures location entry is linked to a valid cardholder
-    FOREIGN KEY (state_code) REFERENCES states(state_code)		-- Ensures the state is valid based on the states table
+    FOREIGN KEY (city_id) REFERENCES city_details(city_id)		-- Ensures the state is valid based on the states table
 );
 
 --Created table to store merchant categories to avoid repitition.
@@ -76,9 +73,9 @@ CREATE TABLE merchant_category (
 
 -- Create the merchant table to store details about businesses where transactions occur
 CREATE TABLE merchant (
-	merchant_id INT PRIMARY KEY,	-- Unique identifier for each merchant
+	merchant_id INT PRIMARY KEY,	        -- Unique identifier for each merchant
 	merchant_name VARCHAR(50) NOT NULL,	-- Merchant's name (e.g., Amazon, Walmart)
-	merchant_cat_id INT,					-- Foreign key linking to the merchant category table
+	merchant_cat_id INT NOT NULL,			-- Foreign key linking to the merchant category table
 	merchant_lat DECIMAL(10,8) NOT NULL,	-- Latitude of the merchant’s location
 	merchant_long DECIMAL(11,8) NOT NULL,	-- Longitude of the merchant’s location
 
@@ -97,4 +94,3 @@ CREATE TABLE transactions (
     FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),		-- Ensures transactions are linked to a valid cardholder
 	FOREIGN KEY (merchant_id) REFERENCES merchant (merchant_id)			-- Ensures transactions are linked to a valid merchant
 );
-
